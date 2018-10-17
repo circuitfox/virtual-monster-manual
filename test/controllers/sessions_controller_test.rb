afa5ec3409147ea23f37add18e72964c37f91015
Chris Stankus <cdstankus@gmail.com>
2018-10-16 22:15:05 -0500

    Add tests for user and session controllers

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,8 +2,34 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get sessions_new_url
+    get login_url
     assert_response :success
   end
 
+  test "should create session" do
+    user = users(:example)
+    post sessions_url, params: { name: user.email, password: "password" }
+    assert_redirected_to root_url
+    assert logged_in_as? user
+  end
+
+  test "should login with username or email" do
+    user = users(:example)
+    post sessions_url, params: { name: user.name, password: "password" }
+    assert logged_in_as? user
+    logout
+    post sessions_url, params: { name: user.email, password: "password" }
+    assert logged_in_as? user
+  end
+
+  test "should flash if login fails" do
+    post sessions_url, params: { name: "", password: "" }
+    assert_not flash.empty?
+  end
+
+  test "should destroy session" do
+    login_as users(:example)
+    delete logout_url
+    assert_not logged_in?
+  end
 end
