3d37bdc0b3ccb16feb504748c219ac869cf84caa
Chris Stankus <cdstankus@gmail.com>
2018-10-26 19:09:37 -0500

    Move dice validation into its own class

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -2,8 +2,7 @@ class Spell < ApplicationRecord
   has_and_belongs_to_many :creatures
 
   validates :name, :description, presence: true
-  validates :dice, format: { with: /\A\d+d\d+\ ?(\+|-)\ ?\d+|\d+d\d+\Z/, 
-    message:"Dice should be in form 1d4 + 8" }
+  validates :dice, dice: { message: "Dice should be in form 1d4 + 8" }
   validates :level, presence: true, numericality: { only_integer: true }
 
 end
